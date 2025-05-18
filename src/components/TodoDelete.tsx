e60975bdfa5179301b74f10d6fd97825f13fed66
Abhijeet Giram <abhijeetgrm@gmail.com>
2025-05-18 16:36:37 +0530

    feat: Updated UI and added check for dashboard route

--- src/components/TodoDelete.tsx
+++ src/components/TodoDelete.tsx
@@ -25,7 +25,7 @@ export default function TodoDelete({
       <div className="relative bg-black border border-gray-200 p-8 rounded-2xl shadow-2xl w-full max-w-md z-10">
         <h2 className="text-xl mb-4">Delete TODO</h2>
         <p>
-          Are you sure you want to delete <b>"{todo.title}"</b>?
+          Are you sure you want to delete <b>{todo.title}</b>?
         </p>
         <div className="flex justify-end space-x-2 mt-4">
           <button
